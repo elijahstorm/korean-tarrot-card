6c54de2cc1cccdf46fc7c528d460cb93742111a8
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-05-27 10:49:46 +0900

    start into animations

--- src/lib/env/intro/intro-content.svelte
+++ src/lib/env/intro/intro-content.svelte
@@ -1,12 +1,16 @@
 <script>
 	import { viewSelectionState } from '$lib/utils/changeState'
 	import { Lang } from '$lib/sources/lang'
-	import { fly } from 'svelte/transition'
+	import { onMount } from 'svelte'
+
+	let animate = true
+
+	onMount(() => (animate = false))
 </script>
 
 <div class="h-full flex flex-col">
 	<div class="flex flex-col flex-1 justify-center">
-		<div class="grid" in:fly={{ delay: 10000 }}>
+		<div class="grid fade-in">
 			<h1
 				class="col-start-1 row-start-1 text-center mb-4 text-5xl font-maruburi blur-sm opacity-70"
 			>
@@ -29,8 +33,22 @@
 
 	<button
 		on:click={viewSelectionState}
-		in:fly
 		class="btn mx-auto text-xl border rounded-full px-8 py-2 text-white border-white opacity-70 transition-all hover:px-12 hover:bg-zinc-700 hover:opacity-100"
 		type="button">{Lang.startButton}</button
 	>
 </div>
+
+<style>
+	@keyframes fade-in {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.fade-in {
+		animation: fade-in 1s ease-out;
+	}
+</style>
