25f200de2d13543634b477937a74ada46ba60a48
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-06-06 20:47:14 +0900

    improve package cmds

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
+const dev = process.env.NODE_ENV === 'production'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/korean-tarrot-card',
+		},
 	},
 }
 
