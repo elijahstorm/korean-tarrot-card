a369aaf770fe80116f588e59106fb30ec754ad38
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-06-06 20:18:36 +0900

    preprocessing config

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,13 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import seqPreprocessor from 'svelte-sequential-preprocessor'
-import { preprocessThrelte } from '@threlte/preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: seqPreprocessor([vitePreprocess(), preprocessThrelte()]),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		outDir: 'docs',
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			precompress: false,
-			fallback: 'index.html',
-		}),
+		adapter: adapter({}),
 		paths: {
 			base: '/korean-tarrot-card',
 		},
